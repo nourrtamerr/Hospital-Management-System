f2cde3220f852d9025433b8d39a315441ba8c092
nourrtamerr <nourtamer202@gmail.com>
2024-11-18 18:34:32 +0200

    Initial Commit

---
+++ Hospital/Migrations/20241028081007_DOB.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hospital.Migrations
+{
+    /// <inheritdoc />
+    public partial class DOB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "DOB",
+                table: "AspNetUsers",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DOB",
+                table: "AspNetUsers");
+        }
+    }
+}
